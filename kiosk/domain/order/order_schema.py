90a9943e12c671d1fa1d427e4c4c1b6a00e2b496
Sang Hyun Kim <22010687@yu.ac.kr>
2023-08-01 15:14:25 +0900

    작업 중

--- kiosk/domain/order/order_schema.py
+++ kiosk/domain/order/order_schema.py
@@ -1,9 +1,27 @@
 from pydantic import BaseModel
 from typing import List
+
+#!#########option###########
+class OptionCreate(BaseModel):
+    option_name: str
+    option_price: int
+    options: List[int] = []
+
+    class Config():
+        from_attributes = True
+class Option(BaseModel):
+    option_pk: int
+    option_name: str
+    option_price: int
+
+    class Config():
+        from_attributes = True
+#!###########################
 class OrderCreate(BaseModel):
     #orderer_id: int
     menu_pk: int
     menu_count: int
+    options: List[int] = []
 
     class Config():
         from_attributes = True
@@ -16,6 +34,7 @@ class OrderResponse(BaseModel):
     menu_count: int
     menu_price: int
     price: int
+    options: List[str] = []
     
     class Config():
         from_attributes = True
@@ -23,9 +42,10 @@ class OrderResponse(BaseModel):
 class OrderSummary(BaseModel):
     orderer_name: str
     orders: List[OrderResponse]
+    total_count: int
     total_price: int
 
     class Config():
-        from_attributes = True    
+        from_attributes = True     
 
 
